56d9523ba3e4870899afbf31a7369252f6c90eb7
Gabi Steele <steeg428@newschool.edu>
2015-11-21 18:19:20 -0500

    Assignment 5 FIXED!

--- Assignment 5/wassign05.js
+++ Assignment 5/wassign05.js
@@ -8,15 +8,17 @@
 
 // {
 // meetingName: ....,
-// meetingAddress: ....,
-// meetingTime: { day: .....,
-//                start time: ....,
-//                type: .....,
-//              }
-// special interest: ....
+// locationName: ....,
+// meetingAddress:....,
 // accessibility: ....,
-// meetingNotes: ....,
-// }
+// meetingDetails: 
+//                { day: .....,
+//                startTime: ....,
+//                endTime: ....,
+//                meetingType: .....,
+//                specialInterest: ....}
+//}
+
 
 
 // parsing meetingAddress && cleaning it
@@ -36,7 +38,6 @@ var $ = cheerio.load(content);
 
 // cleaning Meeting addresses
 // adding 'New York'
-
 function addressClean (dirtyAddress) {
     var cleanAddress = dirtyAddress; 
     
@@ -68,32 +69,53 @@ function nameClean(dirtyNames) {
 
 // still need to clean: special info 
 
+
+function cleanIt (rawData) {
+    var cleanData = {};
+    
+    // CLEAN DAYS
+    cleanData.day = rawData.substr(rawData.indexOf('<b>') - 2, rawData.indexOf('From') - rawData.indexOf('<b>')); 
+    cleanData.day = cleanData.day.replace('<b>','').trim();
+    
+    //CLEAN STARTIME:
+    cleanData.startTime = rawData.substr(rawData.indexOf('</b>') + 5, rawData.indexOf('</b>') - rawData.indexOf('<b>'));
+    cleanData.startTime = cleanData.startTime.replace('<b>to<', '').trim();
+    
+     //CLEAN ENDTIME:
+    cleanData.endTime = rawData.substr(rawData.indexOf('to</b>') + 6, rawData.indexOf('<br>') - rawData.indexOf('</b>'));
+    cleanData.endTime = cleanData.endTime.replace('<br><b>Meeting Type</b>', '').trim();
+    
+    //CLEAN MEETING TYPE:
+    cleanData.meetingType = rawData.substr(rawData.indexOf('Type</b> ') + 8, rawData.indexOf('<br>') - rawData.indexOf('</b>'));
+    cleanData.meetingType = cleanData.meetingType.replace('<', " ").trim();
+    
+    //CLEAN SPECIAL INTEREST
+    cleanData.specialInterest = rawData.substr(rawData.indexOf('Interest</b>', '</b>'), rawData.indexOf('<br>')  - rawData.indexOf('</b>'));
+    cleanData.specialInterest = cleanData.specialInterest.replace('Interest</b>', "");
+    cleanData.specialInterest = cleanData.specialInterest.replace('\r\n\t\t\t \t\t\t<br', "").trim();
+    
+    
+    return cleanData;
+}
+
 var table = $('tbody');
 // creating a varable address to parse the table to find the rows and creating the each function
  $(table).find('tr').each(function(i, elem) {
-    $(elem).find('td').eq(0).each(function(i,elem){
         var obj = new Object; 
         
         // grabbing all the data and pushing it into a single object
          
+        
+        // obj.days = $(elem).find('td').next('td').find('b').eq(0).html();
+        // obj.type = $(elem).find('td').eq(1).text();
         obj.meetingName = nameClean($(elem).find('b').eq(0).text().replace(/\s+/g,' ').trim()); 
         obj.locationName = $(elem).find('h4').eq(0).text().replace(/\s+/g,' ').trim();
         obj.meetingAddress = addressClean($(elem).html().split("<br>")[2].trim());
-        obj.times = $(elem).find('td').eq(1).text(); 
-        obj.days = $(elem).find('td').next('td').find('b').eq(0).html();
-        obj.type = $(elem).find('td').eq(1).text();
-        obj.accessible = $(elem).find('span').eq(0).text().trim();
-        obj.specialInfo = $(elem).find('.detailsBox').eq(0).text().trim(); 
-        obj.directions =  $(elem).find('.GetDirections').eq(0).html();
-        
-        // had trouble grabbing info on: days, meeting type, times
-        // had trouble pushing times into separate object with separate arrays (day, start time, type)
-    
+        obj.accessiblity = $(elem).find('span').eq(0).text().trim();
+        //obj.specialInfo = $(elem).find('.detailsBox').eq(0).text().trim(); 
+        obj.meetingDetails = cleanIt($(elem).find('td').eq(1).html()); 
         addresses.push(obj);
-        
-    });
+   
     console.log(addresses);
 });
-
-
 
